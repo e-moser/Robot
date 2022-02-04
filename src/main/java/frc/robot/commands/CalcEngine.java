c0a5e11f87379e8bc21c8ca9087e306e030a207c
e-moser <78171469+e-moser@users.noreply.github.com>
2022-02-04 13:32:41 -0500

    Created CalcEngine.java with a getVelo function

---
+++ src/main/java/frc/robot/commands/CalcEngine.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+public class CalcEngine {
+    
+  //Everything in Meters
+  public static double velo = 0.0;
+  static double distance = 0.0;
+  static double robotHeight = 0.0;
+  static double targetHeight = 0.0;
+  static double angle = 0.0;
+
+  public static void getVelo(){
+      
+    double b = angle;
+    double a = distance;
+    double c = targetHeight;
+    double d = robotHeight;
+
+    double initial = Math.sqrt(4.9) * a;
+    double firstSqrt = Math.cos(b) * ((a * Math.sin(b)) - (c * Math.cos(b)) + (d * Math.cos(b)));
+    double secondSqrt = (a * Math.cos(b) * Math.sin(b)) - (c * Math.cos(b) * Math.cos(b)) + (d * Math.cos(b) * Math.cos(b));
+    velo = (initial * firstSqrt) / secondSqrt;
+  }
+
+}
