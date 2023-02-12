9f52319e9c1553bd3eab5522c27e4a9b27ca0c40
emoser <78171469+e-moser@users.noreply.github.com>
2023-02-11 23:06:22 -0500

    Revert "Created a GearShifter"
    This reverts commit d8f5d7e39db9d4e8330ec3ca80ca1823786d75f1.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,12 +15,10 @@ import frc.robot.commands.ArmReverse;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ClawSwitch;
 import frc.robot.commands.DriveTank;
-import frc.robot.commands.SwitchGears;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.GearShifter;
 import frc.robot.subsystems.RealDrive;
 import frc.robot.subsystems.SimDrive;
 
@@ -39,7 +37,6 @@ public class RobotContainer {
   private final SimDrive m_simDrive = new SimDrive();
   private final Arm m_arm = new Arm();
   private final Claw m_claw = new Claw();
-  private final GearShifter m_gearShifter = new GearShifter();
 
   private final XboxController leftStick = new XboxController(0);
   private final XboxController rightStick = new XboxController(1);
@@ -77,12 +74,10 @@ public class RobotContainer {
     final JoystickButton codriverA = new JoystickButton(coDriver, XboxController.Button.kA.value);
     final JoystickButton codriverB = new JoystickButton(coDriver, XboxController.Button.kB.value);
     final JoystickButton leftStickTrigger = new JoystickButton(leftStick, 1);
-    final JoystickButton rightStickTrigger = new JoystickButton(rightStick, 1);
 
     codriverA.whileTrue(new ArmForward(m_arm));
     codriverB.whileTrue(new ArmReverse(m_arm));
-    leftStickTrigger.onTrue(new ClawSwitch(m_claw));
-    rightStickTrigger.whileTrue(new SwitchGears(m_gearShifter));
+    leftStickTrigger.whileTrue(new ClawSwitch(m_claw));
   }
 
   /**
